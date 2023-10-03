82a9858f35141124b32b3414b4c872f588dc2ae0
quduss <shuaibqudus33@gmail.com>
2023-10-03 17:13:44 +0100

    Remove unnecessary elif

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,6 @@ def uppercase(str):
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
             x = ord(c) - 32
-        elif ord(c) >= 65 and ord(c) <= 90:
-            x = ord(c)
         else:
             x = ord(c)
         print("{}".format(chr(x)), end='')
