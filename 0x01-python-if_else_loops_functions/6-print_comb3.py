c7e64c8507e3c91ecceacefd07646b452cce7af2
quduss <shuaibqudus33@gmail.com>
2023-10-03 12:45:19 +0100

    Use str.format()

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for x in range(0, 8):
     for y in range(0, 10):
         if y > x:
-            print(f"{x * 10 + y:02d}, ", end='')
-print(89)
+            print("{:02d}, ".format(x * 10 + y), end='')
+print("{}".format(89))
