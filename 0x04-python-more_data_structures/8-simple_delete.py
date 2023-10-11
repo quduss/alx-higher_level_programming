35746a90ad9b4232ca5646d1ff65e61652485c21
quduss <shuaibqudus33@gmail.com>
2023-10-11 16:35:58 +0100

    Use a_dictionary instead of dictionary

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -7,6 +7,6 @@ def simple_delete(a_dictionary, key=""):
     Returns:
         same dictionary
     """
-    if key in dictionary:
+    if key in a_dictionary:
         del a_dictionary[key]
     return a_dictionary
