8d8306546c598b05e526844ac5377b4dc2acc1b7
quduss <shuaibqudus33@gmail.com>
2023-10-25 14:53:35 +0100

    Add __size priv attribute to Square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Square class definition with private attribute __size"""
+
+
+class Square:
+    """Square definition with priv attr __size"""
+    def __init__(self, size):
+        self.__size = size
