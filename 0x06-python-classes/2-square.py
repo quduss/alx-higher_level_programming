610d588af17da28aefe7ec6dfd97ecf803de63a1
quduss <shuaibqudus33@gmail.com>
2023-10-25 15:29:52 +0100

    Instantiate new square with size if it is > 0 and an integer else raise an exception

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square class definition with private attribute __size"""
+
+
+class Square:
+    """Square definition with priv attr __size"""
+    def __init__(self, size=0):
+        """initialize new square object
+        Args:
+            size: size of the square
+        """
+        if (type(size) is not int):
+            raise TypeError("size must be an intger")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
