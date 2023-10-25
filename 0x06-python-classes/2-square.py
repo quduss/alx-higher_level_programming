913b3200400c3a00250042e6c3d9bf987aa003be
quduss <shuaibqudus33@gmail.com>
2023-10-25 15:33:16 +0100

    Fix spelling typo "integr"

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,7 +10,7 @@ class Square:
             size: size of the square
         """
         if (type(size) is not int):
-            raise TypeError("size must be an intger")
+            raise TypeError("size must be an integer")
         if (size < 0):
             raise ValueError("size must be >= 0")
         self.__size = size
