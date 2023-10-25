8675c7872e2074f89c13581e0e92778f7631beda
quduss <shuaibqudus33@gmail.com>
2023-10-25 03:14:22 +0100

    Create public instance method "area"

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Square class definition with private attribute __size"""
+
+
+class Square:
+    """Square definition with priv attr __size"""
+    def __init__(self, size=0):
+        """initialize new square object
+        Args:
+            size: size of the square
+        """
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculate square area
+        Returns:
+            current square area
+        """
+        return self.__size ** 2
