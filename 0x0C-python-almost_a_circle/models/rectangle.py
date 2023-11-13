3aa2a3aa8576dc438504473da6a543bb530fe104
quduss <shuaibqudus33@gmail.com>
2023-11-13 17:04:59 +0100

    Validate attributes of Rectangle instance in setters

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -21,6 +21,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, width):
         """set width"""
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
         self.__width = width
 
     @property
@@ -31,6 +35,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, height):
         """set height"""
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
         self.__height = height
 
     @property
@@ -41,6 +49,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, x):
         """set x"""
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
         self.__x = x
 
     @property
@@ -51,4 +63,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, y):
         """set y"""
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
         self.__y = y
