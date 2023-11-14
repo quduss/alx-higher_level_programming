3a5e1937ac7188673b742289e431ecb4a4c5b3d6
quduss <shuaibqudus33@gmail.com>
2023-11-14 07:19:57 +0100

    Add public instanve method area

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -68,3 +68,7 @@ class Rectangle(Base):
         if y < 0:
             raise ValueError("y must be >= 0")
         self.__y = y
+
+    def area(self):
+        """computes area of rectangle instance"""
+        return self.width * self.height
