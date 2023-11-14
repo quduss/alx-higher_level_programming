3a22f5767079dac9538dc81373f552cdd7dbad62
quduss <shuaibqudus33@gmail.com>
2023-11-14 13:35:34 +0100

    Add public getter and setter size for width and height of square instance

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,3 +16,18 @@ class Square(Rectangle):
         y = self.y
         width = self.width
         return "[Square] ({}) {}/{} - {}".format(id, x, y, width)
+
+    @property
+    def size(self):
+        """public getter size for width or height"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """public setter size for width and height"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
