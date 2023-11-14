3dd0063da4dcf051d922056a038a1de74240b55d
quduss <shuaibqudus33@gmail.com>
2023-11-14 17:39:42 +0100

    Add update method to Square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -31,3 +31,17 @@ class Square(Rectangle):
             raise ValueError("width must be > 0")
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """updating attributes of square instance with args or kwargs"""
+        if args:
+            try:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[2]
+                self.y = args[3]
+            except IndexError:
+                return
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
