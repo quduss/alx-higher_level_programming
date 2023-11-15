d70a9bb3ca18a9a2d1ba1968f091d41e7f4a7f25
quduss <shuaibqudus33@gmail.com>
2023-11-15 15:12:11 +0100

    Add method that returns json string representation of a list of dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,3 +14,10 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        import json
+        return json.dumps(list_dictionaries)
