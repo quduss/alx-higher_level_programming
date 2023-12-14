2f020ccd5b7b8d56d0dfb278e989b3d00629cf77
qudys <shuaibqudus33@gmail.com>
2023-12-14 13:14:55 -0800

    List scores and names from second_table starting from the highest score

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- list records of second_table in database hbtn_0c_0 including only name and score fields
+-- starting from the records with highest score to lowest
+SELECT score, name FROM second_table ORDER BY score DESC;
