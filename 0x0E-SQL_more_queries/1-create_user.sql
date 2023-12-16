4f350d5c6146ca2a820563c00322e4580c108688
qudys <shuaibqudus33@gmail.com>
2023-12-16 04:22:35 -0800

    Add keyword "IDENTIFIED"

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creates user user_0d_1 if it doesn't exist and changes its password
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-ALTER USER 'user_0d_1'@'localhost' BY 'user_0d_1_pwd';
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
