1d2f6b0fb41b17295b4c66f4e08612cdd4dbcfe6
qudys <shuaibqudus33@gmail.com>
2023-12-18 02:18:57 -0800

    List genres with the number of shows in each

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- script list genres with the number of shows that falls in it.
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_genres RIGHT JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
