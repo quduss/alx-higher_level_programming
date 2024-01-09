574ee45350286ba355cead50d2437e73f4ba208f
quduss <shuaibqudus33@gmail.com>
2024-01-09 20:10:52 +0100

    Fix semi-standard errors

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,14 +4,13 @@ const arrLength = process.argv.length;
 
 if (arrLength < 4) {
   console.log(0);
-}
-else {
-  let arr = [];
+} else {
+  const arr = [];
   for (let i = 2; i < arrLength - 1; i++) {
     arr[i - 2] = parseInt(process.argv[i]);
   }
   arr.sort();
-  for (i = arr.length - 2; i >= 0; i--) {
+  for (let i = arr.length - 2; i >= 0; i--) {
     if (arr[i] !== arr[arr.length - 1]) {
       console.log(arr[i]);
       break;
