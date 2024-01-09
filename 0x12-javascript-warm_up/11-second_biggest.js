d6aa10aecd8cccb3b88901a9dd2e33a080710756
quduss <shuaibqudus33@gmail.com>
2024-01-09 20:01:17 +0100

    Print the second largest number from the list of arguments passed

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const arrLength = process.argv.length;
+
+if (arrLength < 4) {
+  console.log(0);
+}
+else {
+  let arr = [];
+  for (let i = 2; i < arrLength - 1; i++) {
+    arr[i - 2] = parseInt(process.argv[i]);
+  }
+  arr.sort();
+  for (i = arr.length - 2; i >= 0; i--) {
+    if (arr[i] !== arr[arr.length - 1]) {
+      console.log(arr[i]);
+      break;
+    }
+  }
+}
