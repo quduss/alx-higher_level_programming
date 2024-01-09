a676b1636841f55cc414e67977e0ffe5aae4831c
quduss <shuaibqudus33@gmail.com>
2024-01-09 18:22:37 +0100

    Print the factorial of the first argument

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const argOne = parseInt(process.argv[2]);
+
+function factorial (n) {
+  if (n === 0 || isNaN(n)) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+console.log(factorial(argOne));
