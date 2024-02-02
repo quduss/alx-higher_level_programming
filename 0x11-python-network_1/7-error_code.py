52c02f3b6a90b731bbaf80fce3c5ef21a8878050
quduss <shuaibqudus33@gmail.com>
2024-02-02 15:40:50 +0100

    Handle HTTPError using requests package

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""sends a request to the url provided and handle HTTPError 
+using requests package"""
+
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    r = requests.get(url)
+    try:
+        r.raise_for_status()
+        print(r.text)
+    except requests.exceptions.HTTPError as errh:
+        print("Error code: {}".format(r.status_code))
