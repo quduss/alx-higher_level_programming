04eaba5ce9bd2b7c8c8bd0c3c6791bbbef600b52
quduss <shuaibqudus33@gmail.com>
2024-02-02 13:29:42 +0100

    Move imports to the top o file

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ print the value of X-Request-Id response header after sending
 a request to the passed url"""
+import sys
+import requests
 
 
 if __name__ == '__main__':
-    import requests
-    import sys
     url = sys.argv[1]
     r = requests.get(url)
     print(r.headers['X-Request-Id'])
