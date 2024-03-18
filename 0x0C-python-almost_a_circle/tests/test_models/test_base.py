dff1abb538ba4a30567b48b2b7dfa534b21776ba
quduss <shuaibqudus33@gmail.com>
2024-03-18 13:53:17 +0100

    Remove test for rectangle

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,27 +4,17 @@ import unittest
 from models.base import Base
 
 
-class TestRectangle(unittest.TestCase):
-    """testing rectangle"""
+class TestBase(unittest.TestCase):
+    """testing Base class"""
 
-    def test_init(self):
-        """testing instantiation"""
+    def test_instance(self):
         b1 = Base()
-        self.assertEqual(b1.id, 1)
-        b2 = Base(1)
-        self.assertEqual(b2.id, 1)
-        b3 = Base(0)
-        self.assertEqual(b3.id, 0)
-        b4 = Base(-1)
-        self.assertEqual(b4.id, -1)
-        b5 = Base()
-        self.assertEqual(b5.id, 2)
-        b6 = Base()
-        self.assertEqual(b6.id, 3)
-        b6.id = 4
-        self.assertEqual(b6.id, 4)
+        self.assertIsInstance(b1, Base)
 
-    def test_isBase(self):
-        """testing instance to be of Base type"""
-        b7 = Base()
-        self.assertTrue(type(b7) is Base)
+    def test_id(self):
+        b2 = Base()
+        self.assertEqual(b2.id, 1)
+        b3 = Base()
+        self.assertEqual(b3.id, 2)
+        b4 = Base(10)
+        self.assertEqual(b4.id, 10)
