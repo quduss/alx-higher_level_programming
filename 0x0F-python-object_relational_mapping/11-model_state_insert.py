7cc2a2deaf995112eae233d4781df1a46c52e9b3
quduss <shuaibqudus33@gmail.com>
2024-04-02 09:25:13 +0100

    Add Louisiana to the session

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -14,5 +14,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     new = State(name='Louisiana')
+    session.add(new)
     session.commit()
     print(new.id)
