ce7a25a42874052a89da38094e925c8528d84b13
quduss <shuaibqudus33@gmail.com>
2024-04-02 09:33:48 +0100

    Shorten line 2

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""a script that adds the State object “Louisiana” to the database hbtn_0e_6_usa"""
+"""a script that adds the State object “Louisiana”
+to the database hbtn_0e_6_usa"""
 
 import sys
 from model_state import Base, State
