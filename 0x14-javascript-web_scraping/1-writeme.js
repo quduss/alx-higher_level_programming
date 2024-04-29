a50bcd5e6e44132abfe9047c73442a47bd1e7411
quduss <shuaibqudus33@gmail.com>
2024-04-29 13:14:02 +0100

    Add space before function parentheses

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(filePath, content, 'utf8', function(error) {
+fs.writeFile(filePath, content, 'utf8', function (error) {
   if (error) {
     console.error(error);
   }
