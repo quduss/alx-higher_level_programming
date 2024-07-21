59a6e4b638135b477c4a36977dda0bc1a2eec812
quduss <shuaibqudus33@gmail.com>
2024-07-21 20:12:15 +0100

    Remove unnecessary code

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,10 +25,6 @@ class Square(Rectangle):
     @size.setter
     def size(self, value):
         """public setter size for width and height"""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
         self.width = value
         self.height = value
 
